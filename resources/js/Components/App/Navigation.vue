93a75321c0aba8a85e5621cfbc745df0b3c4694b
Jeremie <jeremieflahaut@gmail.com>
2024-01-29 12:09:35 +0100

    finish Auth Layout

---
+++ resources/js/Components/App/Navigation.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {Link} from "@inertiajs/vue3";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import CreateNewDropdown from "@/Components/App/CreateNewDropdown.vue";
+import NavLink from "@/Components/NavLink.vue";
+</script>
+
+<template>
+    <nav class="min-w-[200px]">
+        <div class="h-[80px] px-3 items-center gap-3">
+            <Link href="/">
+                <ApplicationLogo
+                    class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
+                />
+            </Link>
+            LaraStorage
+        </div>
+        <div class="px-3">
+            <CreateNewDropdown />
+            <div class="py-3">
+                <NavLink href="/">My Files</NavLink>
+                <NavLink href="/">Shared with me</NavLink>
+                <NavLink href="/">Shared by me</NavLink>
+                <NavLink href="/">Trash</NavLink>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/App/SearchForm.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import TextInput from "@/Components/TextInput.vue";
+import {useForm} from "@inertiajs/vue3";
+
+const form = useForm({
+    search: ''
+});
+</script>
+
+<template>
+    <form class="w-[600px] h-[80px] flex items-center">
+        <TextInput type="text"
+                   class="block w-full mr-2"
+                   v-model="form.search"
+                   autocomplete
+                   placeholder="Search for files and folders"/>
+    </form>
+</template>
+
+<style scoped>
+
+</style>
