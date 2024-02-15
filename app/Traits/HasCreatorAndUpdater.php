7297162faa3a952885a90196f1ac53f8c6a78020
Jeremie <jeremieflahaut@gmail.com>
2024-02-15 12:34:46 +0100

    fix tests

---
+++ app/Traits/HasCreatorAndUpdater.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Auth;
+
+trait HasCreatorAndUpdater
+{
+    protected static function bootHasCreatorAndUpdater()
+    {
+        static::creating(function ($model) {
+            $model->created_by = Auth::id();
+            $model->updated_by = Auth::id();
+        });
+
+        static::updating(function ($model) {
+            $model->updated_by = Auth::id();
+        });
+    }
+}
